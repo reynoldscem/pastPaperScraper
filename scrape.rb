220b58872bb438f6b28aeba31689fbacc7ec7e0f
Charlie Reynolds <reynoldscem@gmail.com>
2015-12-09 06:15:16 +0000

    Grab all years

--- scrape.rb
+++ scrape.rb
@@ -9,25 +9,28 @@ baseURL = 'http://studentnet.cs.manchester.ac.uk/assessment/exam_papers/index.ph
 fullName = false
 
 course = ["COMP11120", "COMP25111"]
+(2006..2014).each do |year|
+  suffix = "?view=&year=#{year}"
 
-page = Nokogiri::HTML(open(baseURL))
+  page = Nokogiri::HTML(open(baseURL))
 
-courseToLinks = Hash.new(Array.new)
+  courseToLinks = Hash.new(Array.new)
 
-page.css('div.indentdiv div.indentdiv').each do |courseEntry|
-  name = courseEntry.css('h2').text
-  name = name.split.first unless fullName
-  courseToLinks[name] = courseEntry.css('ul li a').map do |link|
-    link.get_attribute('href')
+  page.css('div.indentdiv div.indentdiv').each do |courseEntry|
+    name = courseEntry.css('h2').text
+    name = name.split.first unless fullName
+    courseToLinks[name] = courseEntry.css('ul li a').map do |link|
+      link.get_attribute('href')
+    end
   end
-end
 
-courseToLinks = courseToLinks.select{|k,v| course.include?(k)}
+  courseToLinks = courseToLinks.select{|k,v| course.include?(k)}
 
-courseToLinks.each do |courseName, links|
-  FileUtils.mkdir_p courseName
-  links.each do |link|
-    File.write("#{courseName}/#{File.basename(link)}", open(link).read)
+  courseToLinks.each do |courseName, links|
+    FileUtils.mkdir_p "#{courseName}/#{year}-#{year+1}"
+    links.each do |link|
+      File.write("#{courseName}/#{year}-#{year+1}/#{File.basename(link)}", open(link).read)
+    end
   end
 end
 
